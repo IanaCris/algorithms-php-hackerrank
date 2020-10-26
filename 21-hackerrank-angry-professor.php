9d7f060b394f369cd61cd1a90eee1263af541001
IanaCris <ianasousati@gmail.com>
2020-10-25 22:17:12 -0300

    angry-professor

---
+++ 21-hackerrank-angry-professor.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Problem: https://www.hackerrank.com/challenges/angry-professor/problem
+*/
+
+function angryProfessor($k, $a) {
+
+    $numCasos = count($a);
+
+    $alunosCedo = array();
+
+    for ($i=0;$i<$numCasos;$i++) {
+        if ($a[$i] <= 0) {
+            $alunosCedo[] = $a[$i];
+        }
+    }
+
+    if (count($alunosCedo) >= $k) {
+        return "NO";
+    } else {
+        return "YES";
+    }
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $t);
+
+for ($t_itr = 0; $t_itr < $t; $t_itr++) {
+    fscanf($stdin, "%[^\n]", $nk_temp);
+    $nk = explode(' ', $nk_temp);
+
+    $n = intval($nk[0]);
+
+    $k = intval($nk[1]);
+
+    fscanf($stdin, "%[^\n]", $a_temp);
+
+    $a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+    $result = angryProfessor($k, $a);
+
+    fwrite($fptr, $result . "\n");
+}
+
+fclose($stdin);
+fclose($fptr);
